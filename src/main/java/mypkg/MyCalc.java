28528d398d25a8b5bf9578b66739a134110b0e76
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2024-10-10 11:28:26 +0530

    jenkins commit

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -18,12 +18,20 @@ public class MyCalc {
 	{
 		return a/b;
 	}
+	public void greeting()
+	{
+		System.out.println("Welcome to SRE");
+		System.out.println("Welcome to DevOps");
+	}
 
 	public static void main(String[] args) {
 		
 		MyCalc calc = new MyCalc();
 		System.out.println("Sum is: "+calc.sum(20, 10));
 		System.out.println("Diff is: "+calc.diff(20, 10));	
+		System.out.println("Mul is: "+calc.mul(20, 10));	
+		System.out.println("Div is: "+calc.div(20, 10));
+		calc.greeting();
 
 	}
 
